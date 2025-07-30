fc95ab69df0784db62de8a662b2772e13a5dcc06
gib <gib@lifeglug.co.uk>
2025-07-30 10:37:06 +0100

    Basic sample messages just as a starting place

--- frontend/src/store/messages.store.ts
+++ frontend/src/store/messages.store.ts
@@ -19,8 +19,39 @@ type MessagesState = {
   createMessage: (message: MessageInput) => void;
 };
 
+const SAMPLE_MESSAGES = [
+  {
+    id: 0,
+    senderId: 1,
+    recipientId: 2,
+    content: 'Test one',
+    timestamp: '2025-07-30T09:00:00.000Z ',
+  },
+  {
+    id: 1,
+    senderId: 1,
+    recipientId: 2,
+    content: 'Test two',
+    timestamp: '2025-07-30T09:01:00.000Z ',
+  },
+  {
+    id: 2,
+    senderId: 1,
+    recipientId: 2,
+    content: 'Test three',
+    timestamp: '2025-07-30T10:05:00.000Z ',
+  },
+  {
+    id: 3,
+    senderId: 2,
+    recipientId: 1,
+    content: 'Test four',
+    timestamp: '2025-07-30T10:10:00.000Z ',
+  }
+];
+
 const useMessagesStore = create<MessagesState>()((set, get) => ({
-  messages: [],
+  messages: SAMPLE_MESSAGES,
   createMessage: (message: MessageInput) =>
     set((state) => {
       const newMessage: Message = {
